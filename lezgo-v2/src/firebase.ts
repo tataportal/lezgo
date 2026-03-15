cc650c1262456aa0ce4a7fac1285d9bf06110a91
tataportal <tataportal@gmail.com>
2026-03-15 04:40:00 -0700

    chore: force rebuild with env vars

--- lezgo-v2/src/firebase.ts
+++ lezgo-v2/src/firebase.ts
@@ -1,3 +1,4 @@
+// Firebase initialization with env vars from Vercel
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
